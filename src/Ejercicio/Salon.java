e9a22a4f0db1456bd767122a3e34503520718348
Enetz <enetzlourido@gmail.com>
2026-01-06 17:27:58 +0100

    Constructor Salon

--- src/Ejercicio/Salon.java
+++ src/Ejercicio/Salon.java
@@ -5,4 +5,11 @@ public class Salon {
     private Visita[] visitas;
     private int numClie;
     private int numVisi;
+
+    public Salon(int maxNumClientes, int maxNumVisitas) {
+        this.clientes = new Cliente[maxNumClientes];
+        this.visitas = new Visita[maxNumVisitas];
+        this.numClie = 0;
+        this.numVisi = 0;
+    }
 }
