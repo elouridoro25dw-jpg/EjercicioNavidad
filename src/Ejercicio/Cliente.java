40569b2a6386cd861d6fdba530828b770142f5a0
Enetz <enetzlourido@gmail.com>
2026-01-06 01:19:44 +0100

    Constructor

--- src/Ejercicio/Cliente.java
+++ src/Ejercicio/Cliente.java
@@ -5,4 +5,20 @@ public class Cliente {
     private String dni;
     private boolean esSocio;
     private String tipoMembresia;
+
+    // Constructor completo
+    public Cliente(String nombre, String dni, boolean esSocio, String tipoMembresia) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.esSocio = esSocio;
+        this.tipoMembresia = tipoMembresia;
+    }
+
+    // Constructor para los que no sean socios
+    public Cliente(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.esSocio = false;
+        this.tipoMembresia = "";
+    }
 }
